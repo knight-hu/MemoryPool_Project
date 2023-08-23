45b6553768e3d83cd9e00098102a6ff8437960fb
knight-hu <Hu18830278030@163.com>
2023-08-23 11:33:20 +0800

    test

---
+++ build/src/CMakeFiles/Benchmark.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/knight/MyProjects/MemoryPool-master/ConcurrentMemoryPool/src/Benchmark.cpp" "/home/knight/MyProjects/MemoryPool-master/ConcurrentMemoryPool/build/src/CMakeFiles/Benchmark.dir/Benchmark.cpp.o"
+  "/home/knight/MyProjects/MemoryPool-master/ConcurrentMemoryPool/src/UnitTest.cpp" "/home/knight/MyProjects/MemoryPool-master/ConcurrentMemoryPool/build/src/CMakeFiles/Benchmark.dir/UnitTest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../baselib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/knight/MyProjects/MemoryPool-master/ConcurrentMemoryPool/build/baselib/CMakeFiles/baseLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/Benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Benchmark"
+  "Benchmark.pdb"
+  "CMakeFiles/Benchmark.dir/Benchmark.cpp.o"
+  "CMakeFiles/Benchmark.dir/UnitTest.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
